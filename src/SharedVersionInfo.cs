eb36ebe089c5bb498a2476e5aef222beb1bc065f
Stefan Dorner <dorner_stefan@hotmail.com>
2017-02-14 14:20:14 +0100

    Cake Build

---
+++ src/SharedVersionInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+[assembly: AssemblyProduct("Blog Service Core")]
+[assembly: AssemblyCopyright("Copyright © 2017 Dorner Stefan. All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
